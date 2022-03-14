9f67ffaf3c246cf7573db403049449ef0f8ed1fd
Håvard Mjaavatten <havard.mjaavatten@protonmail.com>
2022-03-14 20:20:11 +0100

    Refactor serialization

--- src/xmlserde/ser.nim
+++ src/xmlserde/ser.nim
@@ -22,12 +22,12 @@ func ser*[T: object | tuple](inp: T): XmlNode =
     var
         attrs = newStringTable()
         subs = newSeq[XmlNode]()
+    bind stripGenericParams
     for key, val in inp.fieldPairs:
         const xmlName = xmlNameOf(val, key)
         when hasCustomPragma(val, xmlFlatten):
             subs &= val.ser.subnodes
         elif hasCustomPragma(val, xmlAttr):
-            bind stripGenericParams
             when stripGenericParams(val.type) is Option:
                 if val.isSome: attrs[xmlName] = $val.get
             else:
@@ -35,12 +35,10 @@ func ser*[T: object | tuple](inp: T): XmlNode =
         elif hasCustomPragma(val, xmlText):
             doAssert val.type is Primitive
             subs.add newText($val)
+        elif stripGenericParams(val.type) is Option:
+            if val.isSome: subs.add val.get.ser.formatObjectSer(xmlName)
+        elif stripGenericParams(val.type) is seq:
+            subs &= val.mapIt(it.ser.formatObjectSer(xmlName))
         else:
-            bind stripGenericParams
-            when stripGenericParams(val.type) is Option:
-                if val.isSome: subs.add val.get.ser.formatObjectSer(xmlName)
-            elif stripGenericParams(val.type) is seq:
-                subs &= val.mapIt(it.ser.formatObjectSer(xmlName))
-            else:
-                subs.add val.ser.formatObjectSer(xmlName)
+            subs.add val.ser.formatObjectSer(xmlName)
     "".newXmlTree(subs, attrs)
